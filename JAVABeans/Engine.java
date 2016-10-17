9ff923e06a99787da4354bfcd6bc53008e8c5cd2
Aparna Warade <aparna.warade@outlook.com>
2016-10-17 17:03:21 -0500

    annotation component scan
    Example of auto wiring with annotation component scan

---
+++ JAVABeans/Engine.java
@@ -0,0 +1,11 @@
+package JAVABeans;
+
+public class Engine {
+	private String engineName;
+	public void setEngineName(String engineName) {
+		this.engineName = engineName;
+	}
+	public String getEngineName() {
+		return engineName;
+	}
+}
